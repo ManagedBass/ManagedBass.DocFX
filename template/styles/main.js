04f11d824469cbc845353ef76c2ab2668971eb21
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-07-09 19:04:31 +0530

    Good Table Design

--- template/styles/main.js
+++ template/styles/main.js
@@ -1,1 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+$(document).ready(function ()
+{
+    $("table").addClass("table table-bordered table-striped table-condensed");
+});
