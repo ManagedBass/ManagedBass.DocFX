761b383e08e887b08b01d23b346d3b951dc0de0a
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-07-11 22:52:06 +0530

    Floating Back to Top Button

--- template/styles/main.js
+++ template/styles/main.js
@@ -2,4 +2,18 @@
 $(document).ready(function ()
 {
     $("table").addClass("table table-bordered table-striped table-condensed");
+
+    var amountScrolled = 300;
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > amountScrolled)
+            $('a.back-to-top').fadeIn('slow');
+        else $('a.back-to-top').fadeOut('slow');
+    });
+
+    $('a.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 700);
+
+        return false;
+    })
 });
