bfbfd03aa72c1b268b7c3ff6e4ed02626836cd01
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-07-12 10:06:04 +0530

    Hide Back to Top button while scrolling

--- template/styles/main.js
+++ template/styles/main.js
@@ -1,14 +1,23 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
 $(document).ready(function ()
 {
     $("table").addClass("table table-bordered table-striped table-condensed");
 
+    var scrollTimeout = null;
     var amountScrolled = 300;
 
     $(window).scroll(function () {
-        if ($(window).scrollTop() > amountScrolled)
-            $('a.back-to-top').fadeIn('slow');
-        else $('a.back-to-top').fadeOut('slow');
+        $('a.back-to-top').fadeOut('slow');
+
+        if (scrollTimeout)
+            clearTimeout(scrollTimeout);
+        
+        scrollTimeout = setTimeout(function ()
+        {
+            if ($(window).scrollTop() > amountScrolled)
+                $('a.back-to-top').fadeIn('fast');
+
+            else $('a.back-to-top').fadeOut('fast');
+        }, 500);
     });
 
     $('a.back-to-top').click(function () {
