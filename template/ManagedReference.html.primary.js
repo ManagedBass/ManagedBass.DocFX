e029224ce3f2afe43413e3b523b634885d022e9d
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-10-02 12:53:23 +0530

    Reduce template

--- template/ManagedReference.html.primary.js
+++ template/ManagedReference.html.primary.js
@@ -4,6 +4,5 @@ var mrefCommon = require('./ManagedReference.common.js');
 
 exports.transform = function (model)  {
   model = mrefCommon.transform(model);
-  model._disableToc = model._disableToc || !model._tocPath || (model._navPath === model._tocPath);
   return {item: model};
 }
