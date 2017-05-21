0f57a6469d312f4ba881217b7536c810017a5f58
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-05-21 22:48:56 +0530

    Removed old Template

--- template/ManagedReference.html.primary.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-var mrefCommon = require('./ManagedReference.common.js');
-
-exports.transform = function (model)  {
-  model = mrefCommon.transform(model);
-  return {item: model};
-}
